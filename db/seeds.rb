4edb0c1e9d731db822f7c7f815b7854cd4ab30ad
Matt Berry <mberry41@gmail.com>
2018-01-29 19:43:32 -0700

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,35 @@
+# Areas in the North Swell
+Area.create(name: 'Buckhorn Wash');
+Area.create(name: 'Mexican Mountain Road');
+Area.create(name: 'Mexican Mountain Area');
+Area.create(name: 'River Bridge West');
+Area.create(name: 'Road Draw, Oil Well Flat Road');
+Area.create(name: 'Head of Sinbad North, Eagle Canyon Area');
+
+# Walls in each area
+Wall.create(area_id: 1, name: 'Scenic Byway Wall', description: 'Routes on Scenic Byway Wall climb Navajo Sandstone facing northeast and are shaded during hot summer afternoons.');
+Wall.create(area_id: 1, name: 'Anti-Swell, Bradley Mountain Wear', description: 'Anti-Swell is on the west side of Buckhorn Wash, 0.2 mile south (0.3km) of Scenic Byway Wall.');
+Wall.create(area_id: 1, name: 'Outhouse Tower, Home Haunt', description: ' Approximately 2.4 miles (3.8km) toward the San Rafael River from the Scenic Byway Wall a sign reads...');
+
+Wall.create(area_id: 2, name: 'Bad Obsession Buttress', description: ' Bad Obsession Buttress is the south-facing wall above the San Rafael River west of the bridge at the south entrance...');
+Wall.create(area_id: 2, name: 'Dylan Wall', description: 'For years Dylan Wall has been the private sanctuary of a small fraternity of climbers who furtively visited this Wingate Shangri-la.');
+Wall.create(area_id: 2, name: 'Stock Exchange Wall', description: 'Stock Exchange Wall is the Wingate buttress east of Dylan Wall.');
+
+Wall.create(area_id: 3, name: 'Lone Rock', description: 'Lone Rock is a Wingate Sandstone peak with a Kayenta caprock, designated 6145 on the USGS Mexican Mountain quadrange...');
+Wall.create(area_id: 3, name: 'Peak 6333', description: 'Peak 6333 is the next landform west of Lone Rock and is designated on the USGS Devils Hole Quadrange (7.5 minute topographic series).');
+Wall.create(area_id: 3, name: 'Triple Towers', description: 'Triple Towers are in the Sulphur Canyon, approximately one mile (1.6km) west of Lone Rock. The spires are in view north...');
+
+Wall.create(area_id: 4, name: 'Trojan Man Wall', description: 'Trojan Man is upriver (west) from the bridge over the San Rafael River. To reach, drive 0.4 mile south....');
+Wall.create(area_id: 4, name: 'Sand Worm Buttress, Petrified Wood Tower', description: 'Drive to the parking area for Trojan Man Wall, then hike west along the path on the south side of the San Rafael...');
+Wall.create(area_id: 4, name: 'Halloween Wall', description: 'Halloween Wall is the Wingate buttress west of Bottleneck Peak. Either approach by traversing west...');
+
+Wall.create(area_id: 5, name: 'Bottleneck Peak', description: 'Bottleneck Peak is the commanding landform 6235 feet in elevation (1900m) to the right of the road when viewing south from the San Rafael River.');
+Wall.create(area_id: 5, name: 'Assembly Hall Peak (Northwest Face', description: 'Assembly Hall Peak is 6395 feet in elevation (1949m) and dominates the view left of the road when looking due south from the San...');
+Wall.create(area_id: 5, name: 'Window Blind Peak', description: 'Window Blind Peak is the major landform 1.5 miles (2.4km) due south of Assembly Hall Peak on the same (east) side of the road when traveling south...');
+
+Wall.create(area_id: 6, name: 'Chimney Rock', description: 'Chimney Rock is a large dome north of I-70 (northwest of the exit between Mile Post 122-123). To reach, exit I-70 between mile post....');
+Wall.create(area_id: 6, name: 'Scallyway Point, Mustard Jar, Twin Raven', description: 'Twin Raven is the most prominent of these landforms and is in view 0.75 mile (1.2km) north of I-70 from Mile Post 123.');
+Wall.create(area_id: 6, name: 'Sandcastle, Outpost, Secret Pillar', description: 'Sandcastle and Outpost are on a ridge between Cane Wash and Oil Well Flat. To reach, exit I-70 between Mile Post 122-123.');
+
+# Routes at each wall
+# Area has many routes through walls???
