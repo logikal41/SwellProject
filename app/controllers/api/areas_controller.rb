1c01e120c5d396070225e49082107ef89650e505
Matt Berry <mberry41@gmail.com>
2018-03-01 22:01:01 -0700

    updated the areas controller

--- app/controllers/api/areas_controller.rb
+++ app/controllers/api/areas_controller.rb
@@ -1,17 +1,35 @@
 class Api::AreasController < ApplicationController
 
     def index
-        areas = Area.all
-        render json: areas
+      render json: Area.all
     end
-
+  
     def show
+      render json: Area.find(params[:id])
     end
-
+  
     def update
+      area = Area.find(params[:id])
+      Area.update(area_params)
+      render json: area
     end
-# Hello
+  
+    def create
+      area = Area.new(area_params)
+      if area.save
+          render json: area
+      else
+          render json: {errors: area.errors}, status: :unprocessable_entity
+      end
+    end
+  
     def destroy
+      Area.find(params[:id]).destroy
     end
+  
+    private
+      def area_params
+        params.require(:area).permit(:name, :description)
+      end
 
 end
