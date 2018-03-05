8260431a4c4e325b1b15e9108e35688de96d663b
Matt Berry <mberry41@gmail.com>
2018-03-04 20:04:45 -0700

    updates to bio page

--- client/src/components/Bio.js
+++ client/src/components/Bio.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Header, Container } from 'semantic-ui-react';
 
 class Bio extends React.Component {
+
     render() {
-        return <div> Bio </div>
+        const { name, email, role } = this.props.user
+
+        return (
+            <Container>
+             <Header as='h1' textAlign='center'> User Bio: </Header>
+             <Header as='h3'>Name: {name}</Header>
+             <Header as='h3'>Email: {email}</Header>
+             <Header as='h3'>Role: {role}</Header>
+            </Container>
+        )
     }
 };
 
-export default Bio;
+const mapStateToProps = state => {
+    return {
+        user: state.user,
+    }
+};
+
+export default connect(mapStateToProps)(Bio);
